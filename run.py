d30848e4ba3e3e402e37d7e50644a92369b2502a
AMNlex Amnlex <alexn.alexn@gmail.com>
2024-12-24 23:42:45 +0000

    Added functions for parts of the app

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -13,21 +14,53 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('IT-Survey')
 
 
-sheet1 = SHEET.worksheet('Sheet1')
+# Open the specific sheets
+sheet1 = SHEET.worksheet('Most-popular-technologies')
+sheet2 = SHEET.worksheet('Salary')
+sheet3 = SHEET.worksheet('Databases')
+
+def validate(values):
+    """
+    Validate the user input and return True if valid, False otherwise.
+    """
+    try:
+        int_value = int(values)
+        if 1 <= int_value <= 3:
+            return True
+        else:
+            raise ValueError("Please provide a number between 1 and 3.")
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
 
 
-data = sheet1.get_all_values()
+def get_data_by_user_choice():
 
-print(data)
+    """
+    Get user choice
+    """
+    while True:
+         print("Select a domain of interest from the list below:")
+         print("1: Most popular technologies")
+         print("2: Salary")
+         print("3: Databases")
+         user_input = input("Select a number from 1 to 3: ")
+         if validate(user_input):
+            choice = int(user_input)
+            if choice == 1:
+                return SHEET.worksheet('Most-popular-technologies').get_all_values()
+            elif choice == 2:
+                return SHEET.worksheet('Salary').get_all_values()
+            elif choice == 3:
+                return SHEET.worksheet('Databases').get_all_values()
+            else:
+                print("Please enter a valid number between 1 and 3.")
+         else:
+            print("Invalid input, please try again.")
 
+def main():
+    data = get_data_by_user_choice()
+    print(data)
 
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
